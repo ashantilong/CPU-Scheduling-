9fb65f26f093f274f25cafad95b572cb8b91f7df
ashanti long <ashantilong15@gmail.com>
2021-11-16 18:05:00 -0500

    Creating layout for assigment

---
+++ Makefile
@@ -0,0 +1,9 @@
+CC=gcc
+CFLAG= -c -Wall -g
+GROUPMEMEBER = ashanti&cormac
+
+
+all: CPU-Scheduling-
+
+
+CPU-Scheduling-:
---
+++ cache_block.h
@@ -0,0 +1,28 @@
+//---------------------------------------------------
+//CSCI 340 - Operating System I
+// FAll 2021 (Anthony Leclerc)
+// cache block implentation file
+// CPU Scheduling Project 
+//
+//---------------------------------------------------
+
+//Constant
+
+
+//----------------------------------------------------------------
+//C-Style data structures
+
+
+typedef struct{
+
+
+
+
+}v_struct_t;
+
+
+
+//-----------------------BEGIN (FUNCTION)--------------------------
+//description:
+
+int cache_block();
